da232e905fd5a7a32120b74c051c8e757927194b
Vladis-GitHub <yegoroff_vv@mail.ru>
2025-07-17 04:18:44 +0300

    new_version_17.07.2025

---
+++ guess-number-task/game_v3.py
@@ -0,0 +1,10 @@
+#import game_v2
+from game_v2 import score_game
+from game_v2 import random_predict
+#from game_v2 import score_game, random_predict
+score_game(random_predict) #вызов функции
+n = 10
+z = n
+n = 5
+# This is 
+ 
